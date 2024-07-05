b65c57e93f7e3802dc1cd3eb4a426e18a744198c
will258012 <125041335+will258012@users.noreply.github.com>
2024-07-05 11:00:20 +0800

    Config section code adjustments
    - Move and rename
    - Add "Display vehicle types" config

---
+++ TrainDisplay/Config/ConfigBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using TrainDisplay.Utils;
+namespace TrainDisplay.Config
+{
+    public abstract class ConfigBase<T> where T : ConfigBase<T>, new()
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Load();
+                }
+
+                return instance;
+            }
+        }
+        private static T Load()
+        {
+            if (instance == null)
+            {
+                var configPath = GetConfigPath();
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                try
+                {
+                    if (File.Exists(configPath))
+                    {
+                        using (StreamReader streamReader = new StreamReader(configPath))
+                        {
+                            instance = xmlSerializer.Deserialize(streamReader) as T;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e.ToString());
+                }
+            }
+            return instance ?? (instance = new T());
+        }
+
+        public static void Save()
+        {
+            if (instance == null) return;
+
+            var configPath = GetConfigPath();
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var noNamespaces = new XmlSerializerNamespaces();
+            noNamespaces.Add("", "");
+            try
+            {
+                using (var streamWriter = new StreamWriter(configPath))
+                {
+                    xmlSerializer.Serialize(streamWriter, instance, noNamespaces);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.ToString());
+            }
+        }
+
+        private static string GetConfigPath()
+        {
+            if (typeof(T).GetCustomAttributes(typeof(ConfigPathAttribute), true)
+                .FirstOrDefault() is ConfigPathAttribute configPathAttribute)
+            {
+                return configPathAttribute.Value;
+            }
+            else
+            {
+                Log.Error("ConfigPath attribute missing in " + typeof(T).Name);
+                return typeof(T).Name + ".xml";
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ConfigPathAttribute : Attribute
+    {
+        public ConfigPathAttribute(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
---
+++ TrainDisplay/Config/TrainDisplayConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace TrainDisplay.Config
+{
+    [ConfigPath("TrainDisplay.xml")]
+    public sealed class TrainDisplayConfig : ConfigBase<TrainDisplayConfig>
+    {
+        public int DisplayWidth { get; set; } = 512;
+
+        public string DisplayLanguage { get; set; } = "A_TD_SETTINGS_SYSTEM_LANGUAGE";
+
+        public bool IsTextShrinked { get; set; } = false;
+        public string StationSuffix { get; set; } = "\"駅\",\"站\",\" Station\",\" Sta.\",\" Sta\"";
+        public bool IsTrain { get; set; } = true;
+        public bool IsMetro { get; set; } = true;
+        public bool IsMonorail { get; set; } = true;
+        public bool IsTram { get; set; } = true;
+    }
+}
